70f70aa5a4df0b1485ac398821927fd0be664086
camilafm <kamycfm@gmail.com>
2015-08-31 23:10:29 -0300

    testanto conflitos

--- src/test/java/br/ifce/ordenacao/ordena/bublesortTest.java
+++ src/test/java/br/ifce/ordenacao/ordena/bublesortTest.java
@@ -10,7 +10,7 @@ public class bublesortTest {
 	@Test
 	public void testBubleSort()
 	    {
-	   		int[] vetorEntrada = {10,9,8,7,6,5,4,3,2,1,0};
+	   		int[] vetorEntrada = {10,9,8,7,6,5,4,3,2,1,0}; //teste conflito
 			int[] vetorSaida = {0,1,2,3,4,5,6,7,8,9,10};
 			bublesort bs = new bublesort();
 			
